0f42c7495ef9683ab283d30a3a12663215b1acc9
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-08-29 12:24:45 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/ui/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.restaurantappprojektandroid.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+}

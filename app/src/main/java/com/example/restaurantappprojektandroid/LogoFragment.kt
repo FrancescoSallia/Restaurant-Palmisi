1b147d967b9b59c94a7d510e07fbb1a9e6c55b59
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-08-30 11:16:06 +0200

    Francesco Sallia

--- app/src/main/java/com/example/restaurantappprojektandroid/LogoFragment.kt
+++ app/src/main/java/com/example/restaurantappprojektandroid/LogoFragment.kt
@@ -1,12 +1,14 @@
 package com.example.restaurantappprojektandroid
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
 import com.example.restuarantprojektapp.R
 import com.example.restuarantprojektapp.databinding.FragmentLogoBinding
+
 
 class LogoFragment : Fragment() {
   private lateinit var vb: FragmentLogoBinding
@@ -21,6 +23,8 @@ class LogoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
     }
 }

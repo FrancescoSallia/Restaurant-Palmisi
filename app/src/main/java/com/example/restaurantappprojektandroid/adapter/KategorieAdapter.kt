18d4e0083c46e64ba3115352afb9a8cc1c089f26
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-01 13:25:06 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/adapter/KategorieAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.restaurantappprojektandroid.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restuarantprojektapp.databinding.KategorieKarussellItemBinding
+
+class KategorieAdapter():RecyclerView.Adapter<KategorieAdapter.KategorieViewHolder>() {
+
+    inner class KategorieViewHolder(val binding:KategorieKarussellItemBinding):RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KategorieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: KategorieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}

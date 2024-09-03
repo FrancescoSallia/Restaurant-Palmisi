4f1eb56fe608e8e44b4c6cbf8292f397d6d5b978
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-03 11:14:21 +0200

    Francesco Sallia

--- app/src/main/java/com/example/restaurantappprojektandroid/SpeisekartenFragment.kt
+++ app/src/main/java/com/example/restaurantappprojektandroid/SpeisekartenFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.restaurantappprojektandroid.adapter.KategorieAdapter
+import com.example.restaurantappprojektandroid.ui.MainViewModel
 import com.example.restuarantprojektapp.databinding.FragmentSpeisekartenBinding
 
 class SpeisekartenFragment : Fragment() {
   private lateinit var vb : FragmentSpeisekartenBinding
+  private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,12 @@ class SpeisekartenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getCategories()
+        viewModel.repositoryCategory.observe(viewLifecycleOwner) {
+            vb.rvKategorie.adapter = KategorieAdapter(it)
+
+        }
+
 
     }
 }

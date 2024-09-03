01a288ff6b62a89eb09362dfb1432ae7856712a6
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-03 10:49:51 +0200

    Francesco Sallia

--- app/src/main/java/com/example/restaurantappprojektandroid/remote/MealdbApiService.kt
+++ app/src/main/java/com/example/restaurantappprojektandroid/remote/MealdbApiService.kt
@@ -1,5 +1,6 @@
 package com.example.restaurantappprojektandroid.remote
 
+import com.example.restaurantappprojektandroid.model.Category
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -31,7 +32,7 @@ private val retrofit = Retrofit.Builder()
 interface MealdbApiService {
 
     @GET("categories.php")
-    suspend fun getCategories()
+    suspend fun getCategories(): List<Category>
 
 }
 
---
+++ app/src/main/java/com/example/restaurantappprojektandroid/remote/Repository.kt
@@ -0,0 +1,29 @@
+package com.example.restaurantappprojektandroid.remote
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.restaurantappprojektandroid.model.Category
+
+class Repository(private val api: MealdbApi) {
+
+
+    private val _categorie = MutableLiveData<List<Category>>()
+    val category: LiveData<List<Category>>
+        get() = _categorie
+
+
+    suspend fun getCategories(){
+
+        try {
+            val result = api.retrofitService.getCategories()
+            _categorie.postValue(result)
+        } catch (e: Exception) {
+            Log.i("INFO","schau im Repository nach bei getCategories")
+        }
+
+    }
+
+
+
+}

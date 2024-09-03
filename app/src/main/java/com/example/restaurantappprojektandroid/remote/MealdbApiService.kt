731b182a8f687989c4306bd58c4cb47973c9d12c
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-03 09:59:39 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/remote/MealdbApiService.kt
@@ -0,0 +1,37 @@
+package com.example.restaurantappprojektandroid.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+
+const val BASE_URL = "http://81.169.201.230:8080"
+
+private val logger = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+private val client = OkHttpClient.Builder()
+    .addInterceptor(logger)
+    .build()
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .client(client)
+    .build()
+
+interface MealdbApiService {
+//hier kommen die get funktionen rein
+}
+
+object MealdbApi {
+    val retrofitService: MealdbApiService by lazy { retrofit.create(MealdbApiService::class.java) }
+}

15406d05e5b6e16191f14c9e2e67708af0fcfc22
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-03 13:55:42 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/model/CountryMealResponse.kt
@@ -0,0 +1,5 @@
+package com.example.restaurantappprojektandroid.model
+
+data class CountryMealResponse(
+    val meals: List<CountryMeal>
+)
---
+++ app/src/main/java/com/example/restaurantappprojektandroid/model/CountryResponse.kt
@@ -0,0 +1,5 @@
+package com.example.restaurantappprojektandroid.model
+
+data class CountryResponse(
+    val meals: List<Country>
+)

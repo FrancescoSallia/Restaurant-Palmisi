2b0702eeacd1e68019f78368e28248cbf1191f44
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-11 12:12:39 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/adapter/PersonenanzahlAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.restaurantappprojektandroid.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurantappprojektandroid.model.Personenanzahl
+import com.example.restaurantappprojektandroid.ui.MainViewModel
+import com.example.restuarantprojektapp.databinding.PersonenanzahlItemBinding
+
+class PersonenanzahlAdapter(
+    val dataset : List<Personenanzahl>,
+    val viewModel: MainViewModel
+):RecyclerView.Adapter<PersonenanzahlAdapter.AnzahlGaeste>() {
+    inner class AnzahlGaeste(val vb: PersonenanzahlItemBinding):RecyclerView.ViewHolder(vb.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnzahlGaeste {
+
+        val vb = PersonenanzahlItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AnzahlGaeste(vb)
+    }
+
+    override fun getItemCount(): Int {
+
+        return dataset.size
+    }
+
+    override fun onBindViewHolder(holder: AnzahlGaeste, position: Int) {
+
+        val anzahl = dataset[position]
+
+        holder.vb.tvPersonenanzahlNumber.text = anzahl.anzahlAnGaesten.toString()
+    }
+}
---
+++ app/src/main/java/com/example/restaurantappprojektandroid/model/Personenanzahl.kt
@@ -0,0 +1,5 @@
+package com.example.restaurantappprojektandroid.model
+
+data class Personenanzahl(
+    var anzahlAnGaesten: Int
+)

c263c8cd7560d91e79f1517db7994c0019cf461c
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-09-16 14:01:09 +0200

    Francesco Sallia

---
+++ app/src/main/java/com/example/restaurantappprojektandroid/ui/profile/ReservationDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.example.restaurantappprojektandroid.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.restaurantappprojektandroid.ui.MainViewModel
+import com.example.restuarantprojektapp.R
+import com.example.restuarantprojektapp.databinding.FragmentReservationDetailBinding
+
+class ReservationDetailFragment : Fragment() {
+    private lateinit var vb: FragmentReservationDetailBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        vb = FragmentReservationDetailBinding.inflate(inflater, container, false)
+        return vb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}

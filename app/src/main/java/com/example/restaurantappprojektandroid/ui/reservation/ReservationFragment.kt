dc02b4ffb60d4d9f95a9d52aa193a3fb2854c137
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2024-10-26 19:02:14 +0200

    Francesco Sallia
    Code Cleaned

--- app/src/main/java/com/example/restaurantappprojektandroid/ui/reservation/ReservationFragment.kt
+++ app/src/main/java/com/example/restaurantappprojektandroid/ui/reservation/ReservationFragment.kt
@@ -109,6 +109,7 @@ class ReservationFragment : Fragment() {
             Wann: $zeit
             Bemerkung: $bemerkung
           
+          
                 """
                     )
                     builder.setPositiveButton("OK") { dialog, _ ->
